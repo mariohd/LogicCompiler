33f4e7bff88d62a01192205a6f3ea895b347a863
mariohd <mariohenriquediniz@gmail.com>
2016-09-10 14:09:53 -0300

    adding java class to generate jar

---
+++ src/main/java/MainBuild.java
@@ -0,0 +1,10 @@
+import ui.UserInterface;
+
+/**
+ * Created by MarioDiniz on 10/09/16.
+ */
+public class MainBuild {
+    public static void main(String[] args) {
+        new UserInterface();
+    }
+}
